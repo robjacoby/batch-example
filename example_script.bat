9b116956f7f5914f2f9a64a17c519c17c8dc9b53
Rob Jacoby <rjacoby@localdirectories.com.au>
2014-11-19 16:37:27 +1000

    batch file change

---
+++ example_script.bat
@@ -0,0 +1,10 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+
+SET NL=^
+
+
+SET /p ASCII=<ascii\homer.txt
+
+ECHO Congratulations. You've successfully run your first build on Buildbox!NL!!NL!%ASCII%!NL!!NL!If you need any more help, please email us at hello@buildbox.io!NL!!NL!^<3 Buildbox
